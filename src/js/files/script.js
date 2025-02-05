aed92ddf522edcd668794bb9a4d0c0859fbfcb00
Dron245 <Dron245@yandex.ru>
2025-02-06 00:13:14 +0300

    0602-0013

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,6 +6,25 @@ window.addEventListener("DOMContentLoaded", () => {
 	document.addEventListener("click", documentActions);
 	function documentActions(e) {
 		const targetElement = e.target;
+		console.log(targetElement);
 		
+		 // Проверяем, был ли клик по пункту меню
+		 if (targetElement.closest(".menu__link")) {
+			const menuItem = targetElement.closest(".menu__item");
+	
+			// Закрываем все открытые пункты меню, кроме текущего
+			document.querySelectorAll(".menu__item._active").forEach((item) => {
+			  if (item !== menuItem) {
+				 item.classList.remove("_active");
+			  }
+			});
+	
+			// Переключаем активность текущего пункта
+			menuItem.classList.toggle("_active");
+		 } else {
+			// Закрываем все пункты меню при клике вне меню
+			document.querySelectorAll(".menu__item._active").forEach((item) => {
+			  item.classList.remove("_active");
+			});}
 	}
-})
+});
