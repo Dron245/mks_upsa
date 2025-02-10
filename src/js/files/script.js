073a84d375c0df7b9a93a23b007f05c4626e9d5f
Dron245 <Dron245@yandex.ru>
2025-02-10 10:12:37 +0300

    1002-1012

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,44 +1,44 @@
 // Подключение функционала "Чертоги Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
-window.addEventListener("DOMContentLoaded", () => {
-	document.addEventListener("click", documentActions);
+import { flsModules } from './modules.js';
+window.addEventListener('DOMContentLoaded', () => {
+	document.addEventListener('click', documentActions);
 	function documentActions(e) {
 		const targetElement = e.target;
 		console.log(targetElement);
 
 		//работа с селектами
-		if (targetElement.closest(".selects__item")) {
-			const select = targetElement.closest(".selects__item");
-			select.classList.toggle("_select-open");
+		if (targetElement.closest('.selects__item')) {
+			const select = targetElement.closest('.selects__item');
+			select.classList.toggle('_select-open');
 		}
 	}
 
-	const conditions = document.querySelector(".footer__contraindications");
-	const footerContainer = document.querySelector(".footer__container");
+	const conditions = document.querySelector('.footer__contraindications');
+	const footerContainer = document.querySelector('.footer__container');
 	footerContainer.style.paddingBottom = `${conditions.offsetHeight + 50}px`;
 
 	// плавное открытие саб меню в мобилке
 	function toggleSubmenu(submenu, item) {
-		if (!submenu.style.maxHeight || submenu.style.maxHeight === "0px") {
-			submenu.style.maxHeight = submenu.scrollHeight + "px";
-			item.classList.add("_active");
+		if (!submenu.style.maxHeight || submenu.style.maxHeight === '0px') {
+			submenu.style.maxHeight = submenu.scrollHeight + 'px';
+			item.classList.add('_active');
 		} else {
-			submenu.style.maxHeight = "0px";
-			item.classList.remove("_active");
+			submenu.style.maxHeight = '0px';
+			item.classList.remove('_active');
 		}
 	}
 
-	document.querySelectorAll(".menu__item").forEach((item) => {
-		const link = item.querySelector(".menu__link");
-		const submenu = item.querySelector(".menu__sub-list");
+	document.querySelectorAll('.menu__item').forEach((item) => {
+		const link = item.querySelector('.menu__link');
+		const submenu = item.querySelector('.menu__sub-list');
 
 		if (submenu) {
-			submenu.style.overflow = "hidden"; // Исключаем появление скролла
-			submenu.style.maxHeight = "0px"; // Начальное состояние
+			submenu.style.overflow = 'hidden'; // Исключаем появление скролла
+			submenu.style.maxHeight = '0px'; // Начальное состояние
 
-			link.addEventListener("click", (e) => {
+			link.addEventListener('click', (e) => {
 				if (window.innerWidth <= 768) {
 					e.preventDefault();
 					toggleSubmenu(submenu, item);
@@ -47,11 +47,13 @@ window.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
-	const advantages = document.querySelectorAll(".advantage");
-	 // Преобразуем NodeList в массив
-	 const advantagesArray = Array.from(advantages);
+	const advantages = document.querySelectorAll('.advantage');
+	// Преобразуем NodeList в массив
+	const advantagesArray = Array.from(advantages);
 
-	 if (advantagesArray.length >= 2) {
-		advantagesArray.slice(-2).forEach(el => el.style.borderBottom = "none");
-	 }
+	if (advantagesArray.length >= 2) {
+		advantagesArray.slice(-2).forEach((el) => (el.style.borderBottom = 'none'));
+	}
+
+	document.addEventListener("touchend", function () {}, false);
 });
