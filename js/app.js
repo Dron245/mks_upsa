dd5096bb5eeb6165f89ba94f66ee4e72cb28095f
Dron245 <Dron245@users.noreply.github.com>
2025-02-13 13:06:25 +0000

    deploy: ebe69c7d498e5e527a5f90ac6ce481f476d3f41a

--- js/app.js
+++ js/app.js
@@ -258,7 +258,8 @@
     function extend(target, src) {
         if (target === void 0) target = {};
         if (src === void 0) src = {};
-        Object.keys(src).forEach((key => {
+        const noExtend = [ "__proto__", "constructor", "prototype" ];
+        Object.keys(src).filter((key => noExtend.indexOf(key) < 0)).forEach((key => {
             if (typeof target[key] === "undefined") target[key] = src[key]; else if (ssr_window_esm_isObject(src[key]) && ssr_window_esm_isObject(target[key]) && Object.keys(src[key]).length > 0) extend(target[key], src[key]);
         }));
     }
